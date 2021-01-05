30917b8ba0ccb20f12275b68b54624ae4218a4fb
시혁 박 <exon.indiv@gmail.com>
2021-01-05 22:01:47 +0900

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const settings = require('./settings.json')
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('message', msg => {
+  if (msg.content.match('https?:\/\/discord.gg\/[A-z][A-z][A-z][A-z][A-z][A-z]')) {
+    msg.channel.send('Developer\'s 에서 디스코드 서버 홍보는 금지됩니다.')
+  }
+})
+
+client.login('')
